7264e141ea31348333e9cf9e1127054058ce31b7
FedericoMS <fededeath15@gmail.com>
2023-06-21 00:06:15 -0300

    mejoro perfil

--- src/app/pages/mi-perfil/mi-perfil.component.ts
+++ src/app/pages/mi-perfil/mi-perfil.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Usuario } from 'src/app/clases/usuario';
+import { UserService } from 'src/app/services/user.service';
+import { SpinnerService } from 'src/app/services/spinner.service';
 
 @Component({
   selector: 'app-mi-perfil',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./mi-perfil.component.css']
 })
 export class MiPerfilComponent {
+
+  usuarioActual : Usuario = new Usuario();
+  isLoading : boolean = true;
+
+  constructor(public userService : UserService, private spinner : SpinnerService){
+    this.spinner.show();
+    setTimeout(() => {
+      this.usuarioActual = this.userService.getCurrentUser();
+      this.spinner.hide()
+      this.isLoading = false;
+      
+    }, 2500);
+  }
+
+  ngOnInit(){
+    
+  }
 
 }
