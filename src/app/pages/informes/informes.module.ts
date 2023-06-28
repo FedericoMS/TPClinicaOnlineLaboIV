92ef8b4777f50d49d2a65d4256205419d6d9e9e3
FedericoMS <fededeath15@gmail.com>
2023-06-28 13:02:09 -0300

    sprint 4

--- src/app/pages/informes/informes.module.ts
+++ src/app/pages/informes/informes.module.ts
@@ -6,7 +6,7 @@ import { InformesComponent } from './informes/informes.component';
 
 import { LogUsuariosComponent } from './log-usuarios/log-usuarios.component';
 import { TurnosPorEspecialidadComponent } from './turnos-por-especialidad/turnos-por-especialidad.component';
-
+import { NgChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
@@ -16,7 +16,8 @@ import { TurnosPorEspecialidadComponent } from './turnos-por-especialidad/turnos
   ],
   imports: [
     CommonModule,
-    InformesRoutingModule
+    InformesRoutingModule,
+    NgChartsModule
   ]
 })
 export class InformesModule {}
